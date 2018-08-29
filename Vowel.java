4a3d2169fdf8f78a713d3d17bdaa574c17544c6f
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-29 21:06:03 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class Q
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        char c ='a';
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+        {
+            System.out.print("Vowel");
+        }
+        else
+        {
+            System.out.print("consonant");
+        }
+    }
+}
