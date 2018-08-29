4d5a68d1c6451a7e5f8ee5323055819a27c04d38
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-29 20:44:48 +0530

    Create Pro.java

---
+++ Pro.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class PosNeg
+{
+    public static void main(String args[])
+    {
+        if(10>0)
+        {
+            System.out.print("pos");
+        }
+        else
+        {
+            System.out.print("neg");
+        }
+    }
+}
