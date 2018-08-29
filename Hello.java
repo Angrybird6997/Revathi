3c824508bd1b3033635ba4521cda9f90806394c7
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-29 22:16:42 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Q
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a =s.nextInt();
+        for(int i=0;i<a;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
