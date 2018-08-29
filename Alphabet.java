f5b56a5ef737d1542ad962c01292a8d16b40323c
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-29 21:23:15 +0530

    Create Alphabet.java

---
+++ Alphabet.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class Q
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        char c ='4';
+       
+            if((c>'a'&& c<='z')||( c>'A'&&c<='Z'))
+             {
+            System.out.print("Alphabet");
+             }
+            else
+            {
+            System.out.print("No");
+            }
+        
+    }
+}
