57950df2a53c1881401a5aa21a7a4ee993d11de2
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-29 21:57:39 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Q
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int num=10;
+        int sum=0;
+        for(int i=0;i<=num;i++)
+        {
+            sum+=i;
+        }
+        System.out.print(sum);
+    }
+}
