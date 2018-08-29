a5775309835c7ede8f0b2b3b2f9152c35cb1d359
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-29 21:17:14 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -5,14 +5,21 @@ class Q
     public static void main(String args[])
     {
         Scanner s = new Scanner(System.in);
-        char c ='a';
+        char c ='@';
         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
         {
             System.out.print("Vowel");
         }
         else
         {
+            if((c>'a'&& c<='z')||( c>'A'&&c<='Z'))
+        {
             System.out.print("consonant");
+        }
+            else
+            {
+            System.out.print("Invalid");
+            }
         }
     }
 }
