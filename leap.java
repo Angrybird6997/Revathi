32d08d488b97788bf2e375e8009c5fc4d6afcde9
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-30 14:28:07 +0530

    Update leap.java

--- leap.java
+++ leap.java
@@ -6,7 +6,7 @@ public static void main(String args[])
 {
 Scanner s = new Scanner(System.in);
 int a = s.nextInt();
-if((a%400==0)||(a%4==0)&&(a%100!=0)
+if((a%400==0)||(a%4==0)&&(a%100!=0))
 {
 System.out.print("leap year");
 }
