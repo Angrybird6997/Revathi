bd682e23a2618b5f6b603f9355736566935fdf78
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-30 14:29:55 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class comp2
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        double f=Math.pow(a,b);
+        System.out.print(f);
+        
+    }
+}
+
