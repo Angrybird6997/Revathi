77fb0d89ecde6808dc2ae32cb01184c67a3c6c1b
Angrybird6997 <40392827+Angrybird6997@users.noreply.github.com>
2018-08-30 14:15:26 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class guvi
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a=s.nextInt();
+        String e=String.valueOf(a);
+        System.out.print(e.length());
+    }
+}
